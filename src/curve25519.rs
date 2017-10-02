873953b5cad6d70b3f9f3e6122cd49ab041290ac
Taylor Barrella <tbarrella@gmail.com>
2017-10-01 19:32:18 -0700

    curve25519: gen_pk()

--- src/curve25519.rs
+++ src/curve25519.rs
@@ -3,6 +3,12 @@ use num::{BigUint, One, Zero};
 const BITS: usize = 255;
 const A24: u32 = 121665;
 
+pub fn gen_pk(sk: &[u8]) -> Vec<u8> {
+    let mut u = [0; 32];
+    u[0] = 9;
+    x25519(sk, &u)
+}
+
 /// This is horrible
 /// and insecure
 pub fn x25519(k: &[u8], u: &[u8]) -> Vec<u8> {
@@ -168,5 +174,18 @@ mod tests {
             u = k;
             k = x;
         }
+    }
+
+    #[test]
+    fn test_gen_pk() {
+        let sk_a = "77076d0a7318a57d3c16c17251b26645df4c2f87ebc0992ab177fba51db92c2a";
+        let pk_a = "8520f0098930a754748b7ddcb43ef75a0dbf3a0d26381af4eba4a98eaa9b4e6a";
+        let sk_b = "5dab087e624a8a4b79e17f8b83800ee66f3bb1292618b6fd1c2f8b27ff88e0eb";
+        let pk_b = "de9edb7d7b7dc1b4d35b61c2ece435373f8343c85b78674dadfc7e146f882b4f";
+        let k = "4a5d9d5ba4ce2de1728e3bf480350f25e07e21c947d19e3376f09b3c1e161742";
+        assert_eq!(h2b(pk_a), gen_pk(&h2b(sk_a)));
+        assert_eq!(h2b(pk_b), gen_pk(&h2b(sk_b)));
+        check(k, sk_a, pk_b);
+        check(k, sk_b, pk_a);
     }
 }
