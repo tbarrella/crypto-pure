a81262d53883bdf6228bd5dce85f19bbb48380ba
Taylor Barrella <tbarrella@gmail.com>
2017-12-23 14:28:21 -0800

    chacha20: more block tests

--- src/chacha20.rs
+++ src/chacha20.rs
@@ -239,11 +239,47 @@ mod tests {
         assert_eq!(block_one, block.to_vec());
     }
 
+    fn check_block(expected: &str, key: &[u8], nonce: &[u8], counter: u32) {
+        let chacha20 = ChaCha20::new(key, nonce);
+        let block = &chacha20.block(counter);
+        let expected = h2b(expected);
+        assert_eq!(expected, block.to_vec());
+    }
+
     #[test]
     fn test_block() {
         let chacha20 = ChaCha20::new(&h2b(KEY), NONCE);
         let block = &chacha20.block(1);
         check_serialized_block(block);
+
+        let key = &[0; 32];
+        let nonce = &[0; 12];
+        let expected = "76b8e0ada0f13d90405d6ae55386bd28bdd219b8a08ded1aa836efcc8b770dc7\
+                        da41597c5157488d7724e03fb8d84a376a43b8f41518a11cc387b669b2ee6586";
+        check_block(expected, key, nonce, 0);
+
+        let expected = "9f07e7be5551387a98ba977c732d080dcb0f29a048e3656912c6533e32ee7aed\
+                        29b721769ce64e43d57133b074d839d531ed1f28510afb45ace10a1f4b794d6f";
+        check_block(expected, key, nonce, 1);
+
+        let key = &mut [0; 32];
+        key[31] = 1;
+        let expected = "3aeb5224ecf849929b9d828db1ced4dd832025e8018b8160b82284f3c949aa5a\
+                        8eca00bbb4a73bdad192b5c42f73f2fd4e273644c8b36125a64addeb006c13a0";
+        check_block(expected, key, nonce, 1);
+
+        let key = &mut [0; 32];
+        key[1] = 0xff;
+        let expected = "72d54dfbf12ec44b362692df94137f328fea8da73990265ec1bbbea1ae9af0ca\
+                        13b25aa26cb4a648cb9b9d1be65b2c0924a66c54d545ec1b7374f4872e99f096";
+        check_block(expected, key, nonce, 2);
+
+        let key = &[0; 32];
+        let nonce = &mut [0; 12];
+        nonce[11] = 2;
+        let expected = "c2c64d378cd536374ae204b9ef933fcd1a8b2288b3dfa49672ab765b54ee27c7\
+                        8a970e0e955c14f3a88e741b97c286f75f8fc299e8148362fa198a39531bed6d";
+        check_block(expected, key, nonce, 0);
     }
 
     #[test]
