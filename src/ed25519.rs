27656a249942b3790071145bdde375476a2b92fa
Taylor Barrella <tbarrella@gmail.com>
2017-12-24 13:43:24 -0600

    ed25519: shorten some function names

--- src/ed25519.rs
+++ src/ed25519.rs
@@ -10,7 +10,7 @@ use sha2::{sha512, HashFunction, Sha512};
 /// # Panics
 ///
 /// Panics if `secret_key.len()` is not equal to 32.
-pub fn gen_sign_pk(secret_key: &[u8]) -> [u8; 32] {
+pub fn gen_pk(secret_key: &[u8]) -> [u8; 32] {
     assert_eq!(32, secret_key.len());
     let mut public_key = [0; 32];
     let az = &mut sha512(secret_key);
@@ -1643,7 +1643,7 @@ mod tests {
     use super::*;
     use test_helpers::*;
 
-    fn check_edsa(sk: &str, pk: &str, msg: &str, sig: &str) {
+    fn check(sk: &str, pk: &str, msg: &str, sig: &str) {
         let sk = &h2b(sk);
         let pk = &h2b(pk);
         let msg = &h2b(msg);
@@ -1661,21 +1661,21 @@ mod tests {
         let msg = "";
         let sig = "e5564300c360ac729086e2cc806e828a84877f1eb8e5d974d873e06522490155\
                    5fb8821590a33bacc61e39701cf9b46bd25bf5f0595bbe24655141438e7a100b";
-        check_edsa(sk, pk, msg, sig);
+        check(sk, pk, msg, sig);
 
         let sk = "4ccd089b28ff96da9db6c346ec114e0f5b8a319f35aba624da8cf6ed4fb8a6fb";
         let pk = "3d4017c3e843895a92b70aa74d1b7ebc9c982ccf2ec4968cc0cd55f12af4660c";
         let msg = "72";
         let sig = "92a009a9f0d4cab8720e820b5f642540a2b27b5416503f8fb3762223ebdb69da\
                    085ac1e43e15996e458f3613d0f11d8c387b2eaeb4302aeeb00d291612bb0c00";
-        check_edsa(sk, pk, msg, sig);
+        check(sk, pk, msg, sig);
 
         let sk = "c5aa8df43f9f837bedb7442f31dcb7b166d38535076f094b85ce3a2e0b4458f7";
         let pk = "fc51cd8e6218a1a38da47ed00230f0580816ed13ba3303ac5deb911548908025";
         let msg = "af82";
         let sig = "6291d657deec24024827e69c3abe01a30ce548a284743a445e3680d7db5ac3ac\
                    18ff9b538d16f290ae67f760984dc6594a7c15e9716ed28dc027beceea1ec40a";
-        check_edsa(sk, pk, msg, sig);
+        check(sk, pk, msg, sig);
 
         let sk = "f5e5767cf153319517630f226876b86c8160cc583bc013744c6bf255f5cc0ee5";
         let pk = "278117fc144c72340f67d0f2316e8386ceffbf2b2428c9c51fef7c597f1d426e";
@@ -1713,7 +1713,7 @@ mod tests {
                    0618983f8741c5ef68d3a101e8a3b8cac60c905c15fc910840b94c00a0b9d0";
         let sig = "0aab4c900501b3e24d7cdf4663326a3a87df5e4843b2cbdb67cbf6e460fec350\
                    aa5371b1508f9f4528ecea23c436d94b5e8fcd4f681e30a6ac00a9704a188a03";
-        check_edsa(sk, pk, msg, sig);
+        check(sk, pk, msg, sig);
 
         let sk = "833fe62409237b9d62ec77587520911e9a759cec1d19755b7da901b96dca3d42";
         let pk = "ec172b93ad5e563bf4932c70e1245034c35467ef2efd4d64ebf819683467e2bf";
@@ -1721,6 +1721,6 @@ mod tests {
                    2192992a274fc1a836ba3c23a3feebbd454d4423643ce80e2a9ac94fa54ca49f";
         let sig = "dc2a4459e7369633a52b1bf277839a00201009a3efbf3ecb69bea2186c26b589\
                    09351fc9ac90b3ecfdfbc7c66431e0303dca179c138ac17ad9bef1177331a704";
-        check_edsa(sk, pk, msg, sig);
+        check(sk, pk, msg, sig);
     }
 }
