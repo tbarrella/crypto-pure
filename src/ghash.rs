b0fa417db45ad0f9b8ed6bb998cb817509361de6
Taylor Barrella <tbarrella@gmail.com>
2017-12-24 14:09:21 -0600

    ghash: add tests

--- src/ghash.rs
+++ src/ghash.rs
@@ -149,6 +149,82 @@ mod tests {
     }
 
     #[test]
+    fn test_case_3_4_5_6() {
+        let h = "b83b533708bf535d0aa6e52980d53b78";
+        let a = "";
+        let c = "42831ec2217774244b7221b784d0d49ce3aa212f2c02a4e035c17e2329aca12e\
+                 21d514b25466931c7d8f6a5aac84aa051ba30b396a0aac973d58e091473f5985";
+        let expected = "7f1b32b81b820d02614f8895ac1d4eac";
+        check(expected, h, a, c);
+
+        let a = "feedfacedeadbeeffeedfacedeadbeefabaddad2";
+        let c = &c[..120];
+        let expected = "698e57f70e6ecc7fd9463b7260a9ae5f";
+        check(expected, h, a, c);
+
+        let c = "61353b4c2806934a777ff51fa22a4755699b2a714fcdc6f83766e5f97b6c7423\
+                 73806900e49f24b22b097544d4896b424989b5e1ebac0f07c23f4598";
+        let expected = "df586bb4c249b92cb6922877e444d37b";
+        check(expected, h, a, c);
+
+        let c = "8ce24998625615b603a033aca13fb894be9112a5c3a211a8ba262a3cca7e2ca7\
+                 01e4a9a4fba43c90ccdcb281d48c7c6fd62875d2aca417034c34aee5";
+        let expected = "1c5afe9760d3932f3c9a878aac3dc3de";
+        check(expected, h, a, c);
+    }
+
+    #[test]
+    fn test_case_7_8() {
+        let h = "aae06992acbf52a3e8f4a96ec9300bd7";
+        let a = "";
+        let c = "";
+        let expected = "00000000000000000000000000000000";
+        check(expected, h, a, c);
+
+        let c = "98e7247c07f0fe411c267e4384b0f600";
+        let expected = "e2c63f0ac44ad0e02efa05ab6743d4ce";
+        check(expected, h, a, c);
+    }
+
+    #[test]
+    fn test_case_9_10_11_12() {
+        let h = "466923ec9ae682214f2c082badb39249";
+        let a = "";
+        let c = "3980ca0b3c00e841eb06fac4872a2757859e1ceaa6efd984628593b40ca1e19c\
+                 7d773d00c144c525ac619d18c84a3f4718e2448b2fe324d9ccda2710acade256";
+        let expected = "51110d40f6c8fff0eb1ae33445a889f0";
+        check(expected, h, a, c);
+
+        let a = "feedfacedeadbeeffeedfacedeadbeefabaddad2";
+        let c = &c[..120];
+        let expected = "ed2ce3062e4a8ec06db8b4c490e8a268";
+        check(expected, h, a, c);
+
+        let c = "0f10f599ae14a154ed24b36e25324db8c566632ef2bbb34f8347280fc4507057\
+                 fddc29df9a471f75c66541d4d4dad1c9e93a19a58e8b473fa0f062f7";
+        let expected = "1e6a133806607858ee80eaf237064089";
+        check(expected, h, a, c);
+
+        let c = "d27e88681ce3243c4830165a8fdcf9ff1de9a1d8e6b447ef6ef7b79828666e45\
+                 81e79012af34ddd9e2f037589b292db3e67c036745fa22e7e9b7373b";
+        let expected = "82567fb0b4cc371801eadec005968e94";
+        check(expected, h, a, c);
+    }
+
+    #[test]
+    fn test_case_13_14() {
+        let h = "dc95c078a2408989ad48a21492842087";
+        let a = "";
+        let c = "";
+        let expected = "00000000000000000000000000000000";
+        check(expected, h, a, c);
+
+        let c = "cea7403d4d606b6e074ec5d3baf39d18";
+        let expected = "83de425c5edc5d498f382c441041ca92";
+        check(expected, h, a, c);
+    }
+
+    #[test]
     fn test_case_15_16_17_18() {
         let h = "acbef20579b4b8ebce889bac8732dad7";
         let a = "";
